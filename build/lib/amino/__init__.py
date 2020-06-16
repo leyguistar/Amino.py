761a6315fb0825dfc1bcf09779ceaefc84c259a8
Slimakoi <slimeytoficial@gmail.com>
2020-06-16 18:43:46 +0100

    Release 1.0.4

---
+++ build/lib/amino/__init__.py
@@ -0,0 +1,10 @@
+__title__ = 'Amino.py'
+__author__ = 'Slimakoi'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 20200-2020 Slimakoi'
+__version__ = '1.0.0'
+
+from .client import Client
+from .sub_client import SubClient
+from .lib.util import device, exceptions, headers, helpers, objects
+from .socket import Callbacks, SocketHandler
